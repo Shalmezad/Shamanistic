95f9e5d517d812e66735b5d050eae4bc8b418354
Richard Wardin <shalmezad@gmail.com>
2014-08-14 19:38:00 -0400

    Add response to resize

--- src/init.js
+++ src/init.js
@@ -4,14 +4,18 @@ var Game = {
   canvas: null,
   ctx: null,
   init: function() {
-    Game.canvas = document.getElementsByTagName('canvas')[0];
     Game.ctx = Game.canvas.getContext('2d');
+    Game.ctx.fillStyle="#FF0000";
+    Game.ctx.fillRect(20,20,100,100);
     Game.resize();
   },
   resize: function() {
-
+    Game.canvas.style.height= window.innerHeight+'px';
+    Game.canvas.style.width= window.innerWidth+'px';
+    window.setTimeout(function(){window.scrollTo(0,1);},1);
   }
 };
 
-window.addEventListener('load', Game.load, false);
+Game.canvas = document.getElementsByTagName('canvas')[0];
+window.addEventListener('load', Game.init, false);
 window.addEventListener('resize', Game.resize, false);
